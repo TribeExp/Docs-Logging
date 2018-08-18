9e23f0884b881b1dd2052b10fc23e04f52e2f2f8
Dima Karpovich <karpovich.code@gmail.com>
2018-08-19 00:13:01 +0300

    Documentation for services

--- src/main/java/com/basakdm/excartest/service/UserService.java
+++ src/main/java/com/basakdm/excartest/service/UserService.java
@@ -14,14 +14,12 @@ public interface UserService {
 
     /**
      * Get all user
-     *
      * @return collection of users
      */
     Collection<UserEntity> findAll();
 
     /**
-     * Find car by id
-     *
+     * Find user by id
      * @param id user unique identifier
      * @return Optional with user, if user was founded. Empty optional in opposite case
      */
@@ -46,11 +44,51 @@ public interface UserService {
      * @return  Void.
      */
     void update(UserEntity userEntity);
+
+    /**
+     * Find Password by id
+     * @param userId user unique identifier
+     * @return String with Password, if Password was founded. Empty String in opposite case
+     */
     String getPasswordById(Long userId);
+
+    /**
+     * Find Mail by id
+     * @param email user unique identifier
+     * @return String with Mail, if Mail was founded. Empty Optional in opposite case
+     */
     Optional<UserEntity> findByMail(String email);
+
+    /**
+     * Get id Current user
+     * @return Long with id.
+     */
     long getCurrentUserId() throws Exception;
+
+    /**
+     * Get Current user
+     * @return Optional with User.
+     */
     Optional<UserEntity> getCurrentUser() throws Exception;
+
+    /**
+     * Get Roles user
+     * @return Set<Role>, where user roles are stored.
+     */
     Set<Role> getCurrentUserRoles() throws Exception;
+
+    /**
+     * Create user.
+     * @param email - email current user
+     * @param password - password current user
+     * @return UserEntity - Created user with id.
+     */
     UserEntity createAdmin(String email, String password);
+
+    /**
+     * Get Authority current user
+     * @param user - user, which set the property Authority
+     * @return Set<SimpleGrantedAuthority> z
+     */
     Set<SimpleGrantedAuthority> getAuthority(UserEntity user);
 }
