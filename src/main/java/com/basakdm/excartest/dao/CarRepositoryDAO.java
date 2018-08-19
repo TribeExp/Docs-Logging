d950933b295ee3682baaa59551f8f88cfe0c088a
Dima Karpovich <karpovich.code@gmail.com>
2018-08-19 22:55:49 +0300

    Documentation for CarRepositoryDAO

--- src/main/java/com/basakdm/excartest/dao/CarRepositoryDAO.java
+++ src/main/java/com/basakdm/excartest/dao/CarRepositoryDAO.java
@@ -10,21 +10,51 @@ import java.util.Collection;
 @Repository
 public interface CarRepositoryDAO extends JpaRepository<CarEntity, Long> {
 
-    // returns a list of unactivated car
+    /**
+     * search by unactivated cars
+     * @return returns a list of unactivated car
+     */
     Collection<CarEntity> findAllByIsActivatedFalse();
-    // returns a list of activated car
+
+    /**
+     * search by activated cars
+     * @return  Collection<CarEntity> - a list of activated car
+     */
     Collection<CarEntity> findAllByIsActivatedTrue();
 
-
-    // returns a list with the specified transmission
+    /**
+     * search by type of transmission
+     * @param transmission type of transmission, cars that we want to receive
+     * @return  Collection<CarEntity> - list with the specified transmission
+     */
     Collection<CarEntity> findAllByTransmissionType(Transmission transmission);
-    // returns a list with the specified carBody
+
+    /**
+     * search by type of Body
+     * @param carBody type of car Body, cars that we want to receive
+     * @return  Collection<CarEntity> - list with the specified carBody
+     */
     Collection<CarEntity> findAllByCarBody(CarBody carBody);
-    // returns a list with the specified driveGear
+
+    /**
+     * search by type of drive Gear
+     * @param driveGear type of drive Gear, cars that we want to receive
+     * @return  Collection<CarEntity> - list with the specified driveGear
+     */
     Collection<CarEntity> findAllByDriveGear(DriveGear driveGear);
-    // returns a list with the specified typeEngine
+
+    /**
+     * search by type engine
+     * @param typeEngine type of type engine, cars that we want to receive
+     * @return  Collection<CarEntity> - list with the specified type Engine
+     */
     Collection<CarEntity> findAllByEngineType(TypeEngine typeEngine);
-    // returns a list with the specified typeFuel
+
+    /**
+     * search by type fuel
+     * @param typeFuel type of type fuel, cars that we want to receive
+     * @return  Collection<CarEntity> - list with the specified typeFuel
+     */
     Collection<CarEntity> findAllByFuelType(TypeFuel typeFuel);
 
 
