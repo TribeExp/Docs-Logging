46313043dbe2f784fdd1d2ca4ff3b0449ccf3bde
Dima Karpovich <karpovich.code@gmail.com>
2018-08-19 23:01:10 +0300

    Documentation for RoleRepositoryDAO

--- src/main/java/com/basakdm/excartest/dao/RoleRepositoryDAO.java
+++ src/main/java/com/basakdm/excartest/dao/RoleRepositoryDAO.java
@@ -6,5 +6,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository("roleRepository")
 public interface RoleRepositoryDAO extends JpaRepository<Role, Integer>{
+
+    /**
+     * Returns an object of type Role, by string value
+     * @param role the string to search for
+     * @return  Role
+     */
     Role findByRole(String role);
 }
