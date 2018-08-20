c364494723197ffb8f9eb10a34f0a0d94eea3c36
Dima Karpovich <karpovich.code@gmail.com>
2018-08-20 19:31:19 +0300

    Swagger for OrderController

--- src/main/java/com/basakdm/excartest/controller/OrderController.java
+++ src/main/java/com/basakdm/excartest/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.basakdm.excartest.controller;
 
 import com.basakdm.excartest.dao.OrderRepositoryDAO;
+import com.basakdm.excartest.dto.CarDTO;
 import com.basakdm.excartest.dto.OrderDTO;
 import com.basakdm.excartest.entity.CarEntity;
 import com.basakdm.excartest.entity.OrderEntity;
@@ -8,6 +9,10 @@ import com.basakdm.excartest.request_models.order_models.OrderIdAndPriceAdd;
 import com.basakdm.excartest.service.CarService;
 import com.basakdm.excartest.service.OrderService;
 import com.basakdm.excartest.utils.ConvertOrders;
+import com.basakdm.excartest.utils.ConverterCars;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,6 +26,7 @@ import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Api(value = "Controller for interaction with the methods order", description = "The operations that can be performed with the order table are in this controller")
 @RestController
 @RequestMapping("/order")
 @Slf4j
@@ -37,8 +43,9 @@ public class OrderController {
 
     /**
      * Get all Orders.
-     * @return collection of OrderEntity.
+     * @return collection of OrderDTO.
      */
+    @ApiOperation(value = "Get all Orders.", notes = "")
     @GetMapping("/all")
     public Collection<OrderDTO> findAll(){
         log.info("(/order/all), findAll()");
@@ -50,10 +57,11 @@ public class OrderController {
     /**
      * Find Orders by id
      * @param id orders unique identifier.
-     * @return Optional with order, if order was founded. Empty optional in opposite case.
+     * @return ResponseEntity with OrderDTO, if order was founded. Empty optional in opposite case.
      */
+    @ApiOperation(value = "Find Orders by id", notes = "")
     @GetMapping(value = "/{id}")
-    public ResponseEntity<OrderDTO> findUserById(@PathVariable @Positive Long id){
+    public ResponseEntity<OrderDTO> findUserById(@PathVariable @Positive @ApiParam("id orders unique identifier") Long id){
         log.info("(/order/{id}), findUserById()");
         return orderService.findById(id)
                 .map(ConvertOrders::mapOrder)
@@ -63,10 +71,11 @@ public class OrderController {
     /**
      * Create Order.
      * @param orderEntity params for create a new order.
-     * @return Created order with id.
+     * @return ResponseEntity: Created, Created order with id.
      */
+    @ApiOperation(value = "Create Order.", notes = "")
     @PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody OrderEntity orderEntity){
+    public ResponseEntity<?> create(@RequestBody @ApiParam("orderEntity params for create a new order") OrderEntity orderEntity){
         log.info("(/order/create), create()");
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(orderService.create(orderEntity));
@@ -77,8 +86,9 @@ public class OrderController {
      * @param id order params for delete a order.
      * @return  Void.
      */
+    @ApiOperation(value = "Delete order by id.", notes = "")
     @DeleteMapping ("/delete/{id}")
-    public void delete(@PathVariable @Positive Long id){
+    public void delete(@PathVariable @Positive @ApiParam("id order params for delete a order") Long id){
         log.info("(/order/delete/{id}), delete()");
         orderService.delete(id);
     }
@@ -88,8 +98,9 @@ public class OrderController {
      * @param orderEntity order params for update a order.
      * @return  Void.
      */
+    @ApiOperation(value = "Update order by id.", notes = "")
     @PostMapping ("/update")
-    public void update(@RequestBody OrderEntity orderEntity){
+    public void update(@RequestBody @ApiParam("orderEntity order params for update a order") OrderEntity orderEntity){
         log.info("(/order/update), update()");
         orderService.update(orderEntity);
     }
@@ -99,8 +110,9 @@ public class OrderController {
      * @param orderId order params for update a order.
      * @return  Integer - amount of days.
      */
+    @ApiOperation(value = "Get the number of days how many user use the machine.", notes = "")
     @GetMapping(value = "/getAmountOfDaysById/{orderId}")
-    public Integer getAmountOfDaysById(@PathVariable @Positive Long orderId){
+    public Integer getAmountOfDaysById(@PathVariable @Positive @ApiParam("orderId order params for update a order") Long orderId){
         log.info("(/order/getAmountOfDaysById/{orderId}), getAmountOfDaysById()");
         return orderService.findById(orderId).get().getAmountOfDays();
     }
@@ -110,8 +122,9 @@ public class OrderController {
      * @param orderId order params for find a order, in which we will do the calculation.
      * @return  Date.
      */
+    @ApiOperation(value = "Calculate and get the last day when the user will ride by car.", notes = "")
     @GetMapping(value = "/calcDateFromMomentOfTakingCar/{orderId}")
-    public Date calcDateFromMomentOfTakingCar(@PathVariable @Positive Long orderId){
+    public Date calcDateFromMomentOfTakingCar(@PathVariable @Positive @ApiParam("orderId order params for find a order, in which we will do the calculation") Long orderId){
         log.info("(/order/calcDateFromMomentOfTakingCar/{orderId}), calcDateFromMomentOfTakingCar()");
         Integer amountOfDays = getAmountOfDaysById(orderId);
         log.info("amountOfDays = " + amountOfDays);
@@ -138,8 +151,9 @@ public class OrderController {
      * @param orderId order params for find a order, in which we will do the calculation.
      * @return  Date.
      */
+    @ApiOperation(value = "Calculate and get the last day when the user will ride by car.", notes = "")
     @GetMapping(value = "/getPriceAdd/{orderId}")
-    public Long getPriceAdd(@PathVariable @Positive Long orderId){
+    public Long getPriceAdd(@PathVariable @Positive @ApiParam("orderId order params for find a order, in which we will do the calculation") Long orderId){
         log.info("order/getPriceAdd/{orderId}, getPriceAdd()");
         return orderService.findById(orderId).get().getPriceAdd();
     }
@@ -149,8 +163,9 @@ public class OrderController {
      * @param idAndPrice an object that contains an identifier and a price, to search the table.
      * @return  void.
      */
+    @ApiOperation(value = "Set cell with value-added order.", notes = "")
     @PostMapping ("/setPriceAdd")
-    public void setPriceAdd(@RequestBody OrderIdAndPriceAdd idAndPrice){
+    public void setPriceAdd(@RequestBody @ApiParam("idAndPrice an object that contains an identifier and a price, to search the table") OrderIdAndPriceAdd idAndPrice){
         log.info("order/setPriceAdd, setPriceAdd()");
         Optional<OrderEntity> optionalOrderEntity = orderService.findById(idAndPrice.getOrderId());
         OrderEntity orderEntity = optionalOrderEntity.get();
@@ -166,8 +181,9 @@ public class OrderController {
      * @param carId params for get addPrice.
      * @return  Long.
      */
+    @ApiOperation(value = "Getting for a priceAdd from the order, by carId.", notes = "")
     @GetMapping(value = "/getPriceAddByIdCar/{carId}")
-    public Long getPriceAddByIdCar(@PathVariable @Positive Long carId){
+    public Long getPriceAddByIdCar(@PathVariable @Positive @ApiParam("carId params for get addPrice") Long carId){
         log.info("(order/getPriceAddByIdCar/{carId}), getPriceAddByIdCar()");
         Optional<OrderEntity> optionalOrderEntity = orderService.findByIdCar(carId);
         OrderEntity orderEntity = optionalOrderEntity.get();
@@ -179,13 +195,12 @@ public class OrderController {
      * @param carId the identifier of the machine by which we will search for the order.
      * @return Optional with order, if order was founded. Empty optional in opposite case.
      */
-    //
+    @ApiOperation(value = "Obtaining an Order object, by identifier tsar, with which you can access any cell.", notes = "")
     @GetMapping(value = "/getOrderByIdCar/{carId}")
-    public OrderEntity getOrderEntityByIdCar(@PathVariable @Positive Long carId){
+    public OrderEntity getOrderEntityByIdCar(@PathVariable @Positive @ApiParam("carId the identifier of the machine by which we will search for the order") Long carId){
         log.info("(order/getOrderByIdCar/{carId}), getOrderEntityByIdCar()");
         Optional<OrderEntity> optionalOrderEntity = orderService.findByIdCar(carId);
         OrderEntity orderEntity = optionalOrderEntity.get();
-
         return orderEntity;
     }
 
@@ -194,8 +209,9 @@ public class OrderController {
      * @param carId params for get finPrice.
      * @return  Long.
      */
+    @ApiOperation(value = "Get carEntity to access any field in the table car.", notes = "")
     @GetMapping(value = "/getCarEntityByIdCar/{carId}")
-    public CarEntity getCarEntityById(@PathVariable @Positive Long carId){
+    public CarEntity getCarEntityById(@PathVariable @Positive @ApiParam("carId params for get finPrice") Long carId){
         log.info("(order/getCarEntityByIdCar/{carId}), getCarEntityById()");
         return carServiceImpl.findById(carId).get();
     }
@@ -205,8 +221,9 @@ public class OrderController {
      * @param carId params for get finPrice.
      * @return  Long.
      */
+    @ApiOperation(value = "Get calculated finPrice(final price).", notes = "")
     @GetMapping(value = "/getFinPriceByIdCar/{carId}")
-    public Long getFinPriceByIdCar(@PathVariable @Positive Long carId){
+    public Long getFinPriceByIdCar(@PathVariable @Positive @ApiParam("carId params for get finPrice") Long carId){
         log.info("(order/getFinPriceByIdCar/{carId}), getFinPriceByIdCar()");
         return orderService.findByIdCar(carId).get().getFinPrice();
     }
@@ -216,8 +233,9 @@ public class OrderController {
      * @param carId params for set finPrice.
      * @return  void.
      */
+    @ApiOperation(value = "Set final price(calculate).", notes = "")
     @PostMapping ("/setFinPriceByIdCar/{carId}")
-    public void setFinPriceByIdCar(@PathVariable @Positive Long carId){
+    public void setFinPriceByIdCar(@PathVariable @Positive @ApiParam("carId params for set finPrice") Long carId){
         log.info("(order/setFinPriceByIdCar/{carId}), setFinPriceByIdCar()");
         Long finPrice;
         if (getPriceAddByIdCar(carId) == null) {
